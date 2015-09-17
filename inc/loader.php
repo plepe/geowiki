6db7338f87491662744ad56b9be1d89faf61903e
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2015-09-17 02:18:51 +0200

    Loader: for each map create directory with individual features

--- inc/loader.php
+++ inc/loader.php
@@ -18,7 +18,24 @@ function ajax_load($param) {
   if(!check_param($param))
     return null;
 
-  return json_decode(file_get_contents("{$data_path}{$param['id']}.json"), true);
+  $path = "{$data_path}/{$param['id']}";
+
+  $map_properties = json_decode(file_get_contents("{$path}/map.json"), true);
+
+  $data = array(
+    'id' => $param['id'],
+    'properties' => $map_properties,
+    'features' => array(),
+  );
+
+  $d = opendir($path);
+  while($r = readdir($d)) {
+    if((substr($r, 0, 1) != ".") && ($r != "map.json"))
+      $data['features'][] = json_decode(file_get_contents("{$path}/{$r}"), true);
+  }
+  closedir($d);
+
+  return $data;
 }
 
 function ajax_save($param, $postdata) {
@@ -30,7 +47,23 @@ function ajax_save($param, $postdata) {
       'error' => 'Invalid ID',
     );
 
-  file_put_contents("{$data_path}{$param['id']}.json", $postdata);
+  // create directory for map data
+  $path = "{$data_path}/{$param['id']}";
+  if(!is_dir($path))
+    mkdir($path);
+
+  $data = json_decode($postdata, true);
+
+  if(array_key_exists("properties", $data))
+    file_put_contents("{$path}/map.json", json_readable_encode($data['properties']));
+
+  foreach($data['features'] as $feature) {
+    if(!check_param($feature)) {
+      $feature['id'] = md5(uniqid());
+    }
+
+    file_put_contents("{$path}/_{$feature['id']}.json", json_readable_encode($feature));
+  }
 
   return array(
     'saved' => true,
